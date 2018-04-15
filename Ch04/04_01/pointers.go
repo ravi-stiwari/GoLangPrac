940854c2cb73281183a181a918449143f90ebb54
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-15 19:35:24 +0530

    Pointer

--- Ch04/04_01/pointers.go
+++ Ch04/04_01/pointers.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var p *int
+
+	//panic: runtime error: invalid memory address or nil pointer dereference
+	//fmt.Println("Value of p:", *p)
+
+	if p != nil {
+		fmt.Println("Value of p:", *p)
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var intValue = 42
+	p = &intValue
+
+	if p != nil {
+		fmt.Println("Value of p:", *p)
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var floatValue float64
+	floatValue = 42.13
+	pointer := &floatValue
+	fmt.Println("Float value:", *pointer)
+
+	*pointer = *pointer / 31
+	fmt.Println("Float value using pointer:", *pointer)
+	fmt.Println("Float value using variable:", floatValue)
 }
