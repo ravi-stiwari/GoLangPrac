457c5dfd8a660d9f87efaa93bbae2a039a920da7
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-15 20:38:00 +0530

    Struct in Go

--- Ch04/04_06/structs.go
+++ Ch04/04_06/structs.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
-func main() {
+//Dog is to be used when an Dog object need to be used
+type Dog struct {
+	Breed  string
+	Weight int
+}
 
+func main() {
+	poodle := Dog{"Poodle", 37}
+	fmt.Println(poodle)
+
+	// %+v is used to get described value of an object
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 }
