c4cb685bd5e452c184579e94b6d15cec3622ea74
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-15 20:31:49 +0530

    Maps in Go

--- Ch04/04_05/maps.go
+++ Ch04/04_05/maps.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	
+	states := make(map[string]string)
+	fmt.Println(states)
+
+	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+	fmt.Println(states)
+
+	california := states["CA"]
+	fmt.Println(california)
+
+	delete(states, "OR")
+	fmt.Println(states)
+
+	states["NY"] = "New York"
+
+	for k, v := range states {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+
+	keys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println("Sorted keys :", keys)
+
+	for index := range keys {
+		fmt.Printf("%v : %v\n", keys[index], states[keys[index]])
+	}
 }
