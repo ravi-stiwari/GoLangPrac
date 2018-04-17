5accb607e68d8bd2287ee652a36191e888b99039
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-17 23:41:27 +0530

    Ifthen in Go

--- Ch05/05_01/ifthen.go
+++ Ch05/05_01/ifthen.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	var x float64 = 42
+	//var x float64 = -42
 	var result string
-	
+
+	if x := -42; x < 0 {
+		result = "Less than zero"
+	} else if x == 0 {
+		result = "Greater than to zero"
+	} else {
+		result = "Greater than to zero"
+	}
+	fmt.Println("Result :", result)
+	//x is only available for if else
+	//fmt.Println("Value of x :", x)
 }
