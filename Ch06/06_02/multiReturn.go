5a72f0f5fbc4e8a8c31737b9dd55a27d262001ba
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-18 01:13:25 +0530

    MultiReturn functions and NakedReturn in Functions

--- Ch06/06_02/multiReturn.go
+++ Ch06/06_02/multiReturn.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	name, length := FullName("Ravi Shanker", "Tiwari")
+	fmt.Printf("Fullname: %v, number of chars: %v\n", name, length)
 
+	name1, length1 := FullName("Ankit", "Shukla")
+	fmt.Printf("Fullname: %v, number of chars: %v\n", name1, length1)
+}
+
+//FullName function is for getting the full name and char count by passing first and lastname
+func FullName(f, l string) (string, int) {
+	full := f + " " + l
+	length := len(full)
+	return full, length
+}
+
+//FullNameNakedReturn function is for getting the full name and char count by passing first and lastname
+func FullNameNakedReturn(f, l string) (full string, length int) {
+	full = f + " " + l
+	length = len(full)
+	return
 }
