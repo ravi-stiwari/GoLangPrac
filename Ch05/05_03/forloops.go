e7cff43ef48c5455edeb5798022fb873c8e77f83
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-18 00:18:03 +0530

    For loop as while loop and label and goto There is not while loop in Go

--- Ch05/05_03/forloops.go
+++ Ch05/05_03/forloops.go
@@ -6,8 +6,36 @@ func main() {
 
 	sum := 1
 	fmt.Println("Sum:", sum)
-	
+
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
-	
+
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("Sum:", sum)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	// While loop
+	sum = 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum:", sum)
+		if sum > 200 {
+			goto endofprogram
+		}
+		if sum > 500 {
+			break
+		}
+	}
+	//Labels and goto
+endofprogram:
+	fmt.Println("End of the Program")
 }
