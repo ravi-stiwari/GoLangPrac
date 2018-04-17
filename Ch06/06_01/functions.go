d1a84ee5886d23db497b8305c97a2a453f910887
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-18 00:57:19 +0530

    Function overloading not supported, ... is there for slice as input

--- Ch06/06_01/functions.go
+++ Ch06/06_01/functions.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
+//Function Overloading is not there
 
+func main() {
+	doSomething()
+
+	sum := addValues(23, 54)
+	fmt.Println("Sum :", sum)
+
+	sum = addValues1(23, 54)
+	fmt.Println("Sum :", sum)
+
+	sum = addAllValues(12, 54, 79)
+	fmt.Println("Sum :", sum)
+}
+
+func doSomething() {
+	fmt.Println("Doing Something")
+}
+
+func addValues(value1 int, value2 int) int {
+	return value1 + value2
+}
+
+func addValues1(value1, value2 int) int {
+	return value1 + value2
+}
+
+//Slice of int as args
+func addAllValues(values ...int) int {
+	sum := 0
+	for i := range values {
+		sum += values[i]
+	}
+	fmt.Printf("Type of Argument is %T\n", values)
+	return sum
 }
