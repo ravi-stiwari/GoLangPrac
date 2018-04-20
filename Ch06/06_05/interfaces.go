1c0465b65dec0f9afb9496dca4e5c5ff93ee0716
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-21 00:49:16 +0530

    Evry Val is of any typ & evry typ is implmntatin of atleast one intrface

--- Ch06/06_05/interfaces.go
+++ Ch06/06_05/interfaces.go
@@ -2,10 +2,38 @@ package main
 
 import "fmt"
 
+type Animal interface {
+	Speak() string
+}
+
 type Dog struct {
 }
 
+func (d Dog) Speak() string {
+	return "Woof !!!"
+}
+
+type Cat struct {
+}
+
+func (c Cat) Speak() string {
+	return "Meaw !!!"
+}
+
+type Cow struct {
+}
+
+func (c Cow) Speak() string {
+	return "Moo !!!"
+}
+
 func main() {
-	poodle := Dog{}
+	poodle := Animal(Dog{})
 	fmt.Println(poodle)
+
+	animals := []Animal{Dog{}, Cat{}, Cow{}}
+
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
 }
