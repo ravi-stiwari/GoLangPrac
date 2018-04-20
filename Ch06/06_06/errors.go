46cf483c7bfdc36291bba36e9ea3e31f1172d7b7
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-21 00:59:35 +0530

    Exception handling is done using errors or ok by checking if nil

--- Ch06/06_06/errors.go
+++ Ch06/06_06/errors.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"errors"
 )
 
 func main() {
 	f, err := os.Open("filename.ext")
 
-	// attendance := map[string]bool{
-	// 	"Ann": true,
-	// 	"Mike": true}
+	if err == nil {
+		fmt.Println(f)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	myError := errors.New("My Error String")
+	fmt.Println(myError.Error())
+
+	attendance := map[string]bool{
+		"Ann": true,
+		"Mike": true}
+
+	attended, ok := attendance["Mike"]
+
+	if ok {
+		fmt.Println("Mike Attended ? ", attended)
+	} else {
+		fmt.Println("No info for Mike")
+	}
 }
