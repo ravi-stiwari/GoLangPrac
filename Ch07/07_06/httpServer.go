abe00e565d3f454ea849435fd64ab1c1113e1a7e
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-21 16:02:25 +0530

    Http server using Go ListenAndServer Method need handler named ServeHTTP

--- Ch07/07_06/httpServer.go
+++ Ch07/07_06/httpServer.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
-func main() {
+//Hello has method ServeHttp
+type Hello struct{}
 
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello from Go Web Server</h1>")
+}
+
+func main() {
+	var h Hello
+	err := http.ListenAndServe("localhost:4000", h)
+	checkError(err)
 }
 
 func checkError(err error) {
